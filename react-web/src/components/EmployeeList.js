c321e455b86903b86e2530896077c5fa31602327
Olivia Mo <oliviatrademark@gmail.com>
2018-01-16 14:46:21 +1100

    styled show employees page

--- react-web/src/components/EmployeeList.js
+++ react-web/src/components/EmployeeList.js
@@ -7,7 +7,24 @@ export default function EmployeeList({ employees }) {
       <h1>Employee List</h1>
       {
         employees.map(employee => (
-          <Employee key={employee._id} {...employee}/>
+          <div className="row">
+            <div className="col s1 m3"></div>
+            <div className="col s10 m6">
+              <div className="employee-box grey lighten-1">
+                <span key={employee._id}>
+                  <div className="row employee-list-item">
+                    <div className="col s2 m4"></div>
+                    <div className="col s4 m2">{employee.firstName}</div>
+                    <div className="col s4 m2">{employee.lastName}</div>
+                    <div className="col s2 m4"></div>
+                  </div>
+                </span>
+                <div className="row" key={employee._id}>{employee.contact_number}</div>
+                {/*<Employee key={employee._id} {...employee}/>*/}
+              </div>
+            </div>
+            <div className="col s1 m3"></div>
+          </div>
         ))
       }
     </div>
