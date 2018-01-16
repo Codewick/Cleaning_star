89574963a2d4b228b1a6adc7f8eaf796679f1873
Olivia Mo <oliviatrademark@gmail.com>
2018-01-16 15:20:16 +1100

    styled list clients page

--- react-web/src/components/ClientList.js
+++ react-web/src/components/ClientList.js
@@ -7,7 +7,21 @@ export default function ClientList({ clients }) {
       <h1>Client List</h1>
       {
         clients.map(client => (
-          <Client key={client._id} {...client}/>
+          <div className="row">
+            <div className="col s1 m3"></div>
+            <div className="col s10 m6">
+              <div className="client-box grey lighten-1">
+                <span key={client._id}>
+                    <div className="col s3 m3"></div>
+                    <div className="col s6 m6 client-name">{client.name}</div>
+                    <div className="col s3 m3"></div>
+                    {/*<Client key={client._id} {...client}/>*/}
+                </span>
+              </div>
+            </div>
+            <div className="col s1 m3"></div>
+          </div>
+
         ))
       }
     </div>
