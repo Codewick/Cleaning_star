b889966ca684d9d317e974bdb0a8e14da58e24c8
Olivia Mo <oliviatrademark@gmail.com>
2018-01-19 11:38:53 +1100

    styled show client page, fixed bug in client form where email is not saved

--- react-web/src/components/EmployeeList.js
+++ react-web/src/components/EmployeeList.js
@@ -15,7 +15,7 @@ export default function EmployeeList({ employees }) {
                   <div className="row employee-list-item">
                     <div className="col s1 m3"></div>
 
-                    <div className="card-panel light-blue accent-3 col s10 m6">
+                    <div className="card-panel light-blue accent-4 col s10 m6">
                       <span className="white-text">
                         <div className="row employee-list-item">
                           <p>{employee.firstName} {employee.lastName}</p>
