bd97b8cba6cfcef29606c16a0269bce370cb90fb
Sakshi <correo.sakshi@gmail.com>
2018-01-24 18:10:21 +1100

    add deployment configuration

--- api/models/base.js
+++ api/models/base.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 const db = mongoose.connection;
 
-mongoose.connect('mongodb://localhost/inspections', { useMongoClient: true });
+mongoose.connect('mongodb://star:star@ds111598.mlab.com:11598/cleaning_star', { useMongoClient: true });
 
 module.exports = mongoose;
--- react-web/src/index.js
+++ react-web/src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+console.log(process.env)
+console.log(process.env.NODE_ENV)
+
 ReactDOM.render(<App />, document.getElementById('root'));
