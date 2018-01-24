d3fc470aaacd516a58dda02dfc8a83353ad742e5
Sakshi <correo.sakshi@gmail.com>
2018-01-25 00:00:59 +1100

    deployment settings

--- api/models/base.js
+++ api/models/base.js
@@ -3,6 +3,6 @@ mongoose.Promise = Promise;
 const db = mongoose.connection;
 const config = require('../config');
 
-mongoose.connect(config.apiLink, { useMongoClient: true });
+mongoose.connect('mongodb://star:star@ds111598.mlab.com:11598/cleaning_star', { useMongoClient: true });
 
 module.exports = mongoose;
--- api/server.js
+++ api/server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = require('./app');
 const port = 7000;
 app.listen(port, () => {
